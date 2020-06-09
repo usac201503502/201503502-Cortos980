c1ac233c8e46a711c8e57c985b3dd21d4a7719f0
usac201503502 <3000808400101@ingenieria.usac.edu.gt>
2020-06-09 12:10:48 -0600

    primer commit primera función

---
+++ corto1/corto1.py
@@ -0,0 +1,19 @@
+#funcion que genera la secuencia collatz de un número n
+def collatzf(n):
+    num = n #se asigna la entrada de a otra variable
+    listacollatz = [num]
+    while (num != 1): #mientras el número no se reduzca hasta 1 continuar operando
+        if num%2 == 0 : #si el número es par
+            #print("es par")
+            num /= 2  #se divide entre 2
+            listacollatz.append(num) #se agrega a la lista
+            
+        else: #si el número es impar
+            #print("es impar")
+            num *= 3 #se multiplica por 3
+            num += 1 #se le suma 1
+            listacollatz.append(num) #se agrega a la lista
+    return  listacollatz   
+
+a = int(input())
+print(str(collatzf(a)))
